e82bb97e2998bc5633f1ceb405157efdaa3f3d98
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2024-02-02 21:52:02 +0530

    Updated App and Index files

--- astrology_react/src/App.jsx
+++ astrology_react/src/App.jsx
@@ -1,11 +1,29 @@
-import './App.css'
+import "./App.css";
+import Navbar from "./Components/Navigation/Navbar";
+import Header from "./Components/Header/Header";
+import About from "./Components/About/About";
+import Horoscope from "./Components/HoroScope/Horoscope";
+import Ourservice from "./Components/OurService/Ourservice";
+import WhyChoseUs from "./Components/WhyChoseUs/WhyChoseUs";
+import Feedback from "./Components/FeedbackClient/Feedback";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
-
   return (
     <>
+      <div className="app">
+        <Navbar />
+        {/* <Pages/> */}
+        <Header />
+        <About />
+        <Horoscope/>
+        <Ourservice/>
+        <WhyChoseUs/>
+        <Feedback/>
+        <Footer/>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
