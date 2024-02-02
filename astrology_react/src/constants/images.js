929b6595a4b6ceab9f140248ba65ca50f60a0c23
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2024-02-02 21:54:34 +0530

    updated the Asset export files

---
+++ astrology_react/src/constants/images.js
@@ -0,0 +1,60 @@
+import profile from "../assets/profile.jpg";
+import logo from "../assets/logo.svg";
+import wishlist from "../assets/wishlist.svg";
+import cart from "../assets/cart.svg";
+import hand_bg from "../assets/hand_bg.png";
+import hand from "../assets/hand.png";
+import about from "../assets/about.jpg";
+import about_exp from "../assets/about.svg";
+import underline from "../assets/underline.png";
+import h1 from "../assets/h1.svg";
+import h2 from "../assets/h2.svg";
+import h3 from "../assets/h3.svg";
+import h4 from "../assets/h4.svg";
+import h5 from "../assets/h5.svg";
+import h6 from "../assets/h6.svg";
+import h7 from "../assets/h7.svg";
+import h8 from "../assets/h8.svg";
+import h9 from "../assets/h9.svg";
+import h10 from "../assets/h10.svg";
+import h11 from "../assets/h11.svg";
+import h12 from "../assets/h12.svg";
+import service1 from "../assets/service_img1.jpg";
+import service2 from "../assets/service_img2.png";
+import service3 from "../assets/service_shape.png";
+import icon1 from "../assets/icon1.png";
+import icon2 from "../assets/icon2.png";
+import choose from "../assets/choose.svg";
+import logo_white from "../assets/logo_white.svg";
+
+
+export default {
+  profile,
+  logo,
+  wishlist,
+  cart,
+  hand_bg,
+  hand,
+  about,
+  underline,
+  about_exp,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  h7,
+  h8,
+  h9,
+  h10,
+  h11,
+  h12,
+  service1,
+  service2,
+  service3,
+  icon1,
+  icon2,
+  choose,
+  logo_white
+};
